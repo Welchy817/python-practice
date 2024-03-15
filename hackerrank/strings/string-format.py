232f85a42d814ceaf7b19a0edf09aa45f23ece93
Mike Welch <mkwelch817@gmail.com>
2024-03-15 15:59:20 -0400

    Hackerrank Python string formatting

---
+++ hackerrank/strings/string-format.py
@@ -0,0 +1,8 @@
+def print_formatted(number):
+    # your code goes here
+    padding = len(bin(number)[2:])
+    for i in range(1, number + 1):
+        print(f"{i:{' '}{'>'}{padding}} {oct(i)[2:]:{' '}{'>'}{padding}} {hex(i)[2:].upper():{' '}{'>'}{padding}} {bin(i)[2:]:{' '}{'>'}{padding}}")
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
